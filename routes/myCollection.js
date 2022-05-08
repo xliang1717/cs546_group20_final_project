262b3b6dad678f9b23882ad5a2ae0f66f5c0cb2c
Shan-L <sleng@stevens.edu>
2022-05-08 12:53:20 -0400

    update for tota11y

--- routes/myCollection.js
+++ routes/myCollection.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const method = require('../method');
 const myCollectionData = method.myCollection;
-const parklotData= method.parklot;
+const parklotData = method.parklot;
 const commentData = method.comment;
 
 //TODO: Add validation for all below APIs.
@@ -51,19 +51,19 @@ router.delete('/:id', async (req, res) => {
 
 router.get('/favorite/:id', async (req, res) => {
     let id = req.params.id;
-    req.session.user.userId="626dcbb98ce6dca27a55ea18";
+    req.session.user.userId = "626dcbb98ce6dca27a55ea18";
     try {
-      
+
         await myCollectionData.addParkingLotToUserCollectionTemp(id, req.session.user.userId);
         let parkinglot = await parklotData.get(id);
         let commentList = await commentData.getAllCommentsOfTheOneParkLotID(id);
-        res.render('pages/parkinglot', { title: parkinglot.parkLotname, parkinglotInfo: parkinglot, commentlistInfo:commentList,content:"Parking lot has been added to my collection successfully." });
+        res.render('pages/parkinglot', { title: parkinglot.parkLotname, parkinglotInfo: parkinglot, commentlistInfo: commentList, content: "Parking lot has been added to my collection successfully." });
     } catch (e) {
-      let parkinglot = await parklotData.get(id);
-      let commentList = await commentData.getAllCommentsOfTheOneParkLotID(id);
-      res.status(500).render('pages/parkinglot',{ title: parkinglot.parkLotname, parkinglotInfo: parkinglot, commentlistInfo:commentList,error: e});
+        let parkinglot = await parklotData.get(id);
+        let commentList = await commentData.getAllCommentsOfTheOneParkLotID(id);
+        res.status(500).render('pages/parkinglot', { title: parkinglot.parkLotname, parkinglotInfo: parkinglot, commentlistInfo: commentList, error: e });
     }
-  });
-  
+});
+
 
 module.exports = router;
