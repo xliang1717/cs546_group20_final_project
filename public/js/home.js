26e61cce6309a6d800dbde11ae2b3a482ad80b55
xliang1717 <xliang17@stevens.edu>
2022-05-08 15:35:53 -0400

    # Solve browser error & letter space changes

--- public/js/home.js
+++ public/js/home.js
@@ -1,45 +1,46 @@
+if (window.location.href == "http://localhost:3000/") {
+    window.onload = function () {
+        var wrap = document.getElementById('wrap'),
 
-    window.onload=function(){
-        var wrap=document.getElementById('wrap'),
-        pic=document.getElementById('pic').getElementsByTagName("li"),
-        list=document.getElementById('list').getElementsByTagName('li'),
-        index=0,
-        timer=null;
+            pic = document?.getElementById('pic')?.getElementsByTagName("li"),
+            list = document?.getElementById('list')?.getElementsByTagName('li'),
+            index = 0,
+            timer = null;
 
         // 定义并调用自动播放函数
         timer = setInterval(autoPlay, 2000);
 
         // 鼠标划过整个容器时停止自动播放
         wrap.onmouseover = function () {
-        clearInterval(timer);
+            clearInterval(timer);
         }
 
         // 鼠标离开整个容器时继续播放至下一张
         wrap.onmouseout = function () {
-        timer = setInterval(autoPlay, 2000);
+            timer = setInterval(autoPlay, 2000);
         }
         // 遍历所有数字导航实现划过切换至对应的图片
         for (var i = 0; i < list.length; i++) {
-        list[i].onmouseover = function () {
-        clearInterval(timer);
-        index = this.innerText - 1;
-        changePic(index);
-        };
+            list[i].onmouseover = function () {
+                clearInterval(timer);
+                index = this.innerText - 1;
+                changePic(index);
+            };
         };
 
-        function autoPlay () {
-        if (++index >= pic.length) index = 0;
-        changePic(index);
+        function autoPlay() {
+            if (++index >= pic.length) index = 0;
+            changePic(index);
         }
 
         // 定义图片切换函数
-        function changePic (curIndex) {
-        for (var i = 0; i < pic.length; ++i) {
-        pic[i].style.display = "none";
-        list[i].className = "";
-        }
-        pic[curIndex].style.display = "block";
-        list[curIndex].className = "on";
+        function changePic(curIndex) {
+            for (var i = 0; i < pic.length; ++i) {
+                pic[i].style.display = "none";
+                list[i].className = "";
+            }
+            pic[curIndex].style.display = "block";
+            list[curIndex].className = "on";
         }
 
     };
@@ -63,3 +64,5 @@
         document.getElementById('wrap').style.display = 'none';
         document.getElementById('show3').style.display = 'block';
     }
+
+}
